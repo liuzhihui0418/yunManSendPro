a86ea57ae4eff5f40ff1c530fabacec3e8bcd5c3
liuzhihui0418 <1101225081@qq.com>
2025-12-04 21:09:24 +0800

    前端下单页

--- paper_backend/settings.py
+++ paper_backend/settings.py
@@ -107,13 +107,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = "UTC"
+# 2. 把时区改成亚洲/上海 (这才是关键！)
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
